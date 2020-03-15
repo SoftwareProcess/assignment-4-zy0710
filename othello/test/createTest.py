162e35c966e246e370494bf78787f993b7cafa52
summer <yzz0187@auburn.edu>
2020-03-14 21:44:32 -0500

    Red Light |

--- othello/test/createTest.py
+++ othello/test/createTest.py
@@ -180,6 +180,15 @@ class CreateTest(TestCase):
         actualResult = create(self.inputDictionary)
         self.assertEqual(expectResult,actualResult)
     
+    def test100_040HighBoundBlank(self):
+        self.setLight(3)
+        self.setDark(4)
+        self.setBlank(9)
+        self.setSize(10)
+        expectResult = {'board': [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 3, 4, 9, 9, 9, 9, 9, 9, 9, 9, 4, 3, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9], 'tokens': {'light': 3, 'dark': 4, 'blank': 9}, 'status': 'ok', 'integrity': '5b4c82af0cf6a72ab1938b8e5a3c1ce413b9db583d0f974703954427413021d0'}
+        actualResult = create(self.inputDictionary)
+        self.assertEqual(expectResult,actualResult)
+    
     
     
     
