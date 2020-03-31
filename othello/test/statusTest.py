2091021e829c2ca5ec96ee2e97463a817a0c5efc
summer <yzz0187@auburn.edu>
2020-03-31 16:48:22 -0500

    Red Light |

--- othello/test/statusTest.py
+++ othello/test/statusTest.py
@@ -533,7 +533,7 @@ class StatusTest(TestCase):
         self.setLight(2)
         self.setDark(2)
         self.setBlank(0)
-        self.setBoard([0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,0,0,0,0,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
+        self.setBoard([0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,0,0,0,0,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
         self.setIntegrity('e50f93033edd2b27fd1c54631a4b574e545df9e8c06e0b4f74ca94841a4ab6c4')
         expectResult = {'status': 'error: dark and light have the same value'}
         actualResult = status(self.inputDictionary)
