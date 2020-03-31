d9eaf41763f9b1dd654249600fa15e370fa44575
summer <yzz0187@auburn.edu>
2020-03-31 15:03:48 -0500

    Red Light |

--- othello/test/statusTest.py
+++ othello/test/statusTest.py
@@ -499,3 +499,13 @@ class StatusTest(TestCase):
         expectResult = {'status': 'error: long integrity'}
         actualResult = status(self.inputDictionary)
         self.assertEqual(expectResult,actualResult)
+    
+    def test_942NonHexCharactersIntegrity(self):
+        self.setLight(1)
+        self.setDark(2)
+        self.setBlank(3)
+        self.setBoard([3,3,3,3,3,3,3,3,3,3,3,3,3,3,1,2,3,3,3,3,2,1,3,3,3,3,3,3,3,3,3,3,3,3,3,3])
+        self.setIntegrity('f01977c17f801c43eeb13fb9f74a49bd0c761db3cdffe01510f47ddd23ab465$')
+        expectResult = {'status': 'error: non hex characters integrity'}
+        actualResult = status(self.inputDictionary)
+        self.assertEqual(expectResult,actualResult)
