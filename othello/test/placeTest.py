876b9da9f7f1cb99f3ca75dd72c4eb2c711dea2c
summer <yzz0187@auburn.edu>
2020-04-27 06:53:36 -0500

    Green Light |

--- othello/test/placeTest.py
+++ othello/test/placeTest.py
@@ -353,6 +353,17 @@ class StatusTest(TestCase):
         actualResult = place(self.inputDictionary)
         self.assertEqual(expectResult,actualResult)
     
+    def test_923NullBlank(self):
+        self.setLight(1)
+        self.setDark(2)
+        self.setBlank('')
+        self.setBoard([0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,2,0,0,0,0,2,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
+        self.setLocation('2:3')
+        self.setIntegrity('6c3ec0129f5e128f48e2541bd6663a52a825c35f99b9a69d9593f2fc44b0bb4b')
+        expectResult = {'status': 'error: null blank'}
+        actualResult = place(self.inputDictionary)
+        self.assertEqual(expectResult,actualResult)
+    
     
     
     
