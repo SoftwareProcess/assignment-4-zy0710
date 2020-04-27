0e2e97d4b155feb5ecfa12b69a0ca9c0806fbc83
summer <yzz0187@auburn.edu>
2020-04-27 07:05:54 -0500

    Green Light |

--- othello/test/placeTest.py
+++ othello/test/placeTest.py
@@ -440,7 +440,16 @@ class StatusTest(TestCase):
         actualResult = place(self.inputDictionary)
         self.assertEqual(expectResult,actualResult)
     
-    
+    def test_941LongIntegrity(self):
+        self.setLight(1)
+        self.setDark(2)
+        self.setBlank(3)
+        self.setBoard([3,3,3,3,3,3,3,3,3,3,3,3,3,3,1,2,3,3,3,3,2,1,3,3,3,3,3,3,3,3,3,3,3,3,3,3])
+        self.setLocation('2:3')
+        self.setIntegrity('f01977c17f801c43eeb13fb9f74a49bd0c761db3cdffe01510f47ddd23ab465a00')
+        expectResult = {'status': 'error: long integrity'}
+        actualResult = place(self.inputDictionary)
+        self.assertEqual(expectResult,actualResult)
     
     
     
